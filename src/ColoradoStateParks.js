9ce4b763f9ac59f503fa4b09602c810fd491be89
seenso <>
2021-11-22 13:36:35 -0900

    Completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,23 @@
 import React from "react";
-import howManyParks from "./parks/howManyParks";
+import HowManyParks from "./parks/howManyParks";
+import MesaVerdePark from "./parks/MesaVerde";
+import {trees as parkTrees, wildlife as parkWildlife } from "./parks/RockyMountain";
+import * as RMFunctions from "./parks/RockyMountain";
+
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  // howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  // return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <h1>Colorado State Parks!</h1>
+      {/* <HowManyParks /> */}
+      <MesaVerdePark />
+      
+    </div>
+  )
 }
+
+
+export default ColoradoStateParks;
